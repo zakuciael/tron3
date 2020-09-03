bd11d5e2f655b68ef0b204f735dd9cb5fc7610b9
Krzysztof Saczuk <zakuciael@outlook.com>
2020-09-03 16:10:40 +0200

    Added "settings raw" sub command.

---
+++ src/commands/settings/RawSettingsSubCommand.ts
@@ -0,0 +1,29 @@
+import {SubCommand, Usage} from "../../commander/Command";
+import {ConfigManager} from "../../utils/ConfigManager";
+import {EmbedBuilder} from "../../utils/EmbedBuilder";
+import { Message } from "discord.js";
+
+export class RawSettingsSubCommand extends SubCommand {
+    command: string = "settings";
+    subcommand: string = "raw";
+    usage: Usage = {
+        syntax: "settings raw",
+        description: "Displays raw settings"
+    };
+
+    async execute(msg: Message, args: string[], config: ConfigManager): Promise<void> {
+        const embed = EmbedBuilder.getCommandEmbed(msg.member!);
+        embed.setTitle("Raw Settings");
+
+        let copy = JSON.parse(JSON.stringify(config.raw()));
+        delete copy.token;
+
+        embed.setDescription("```json\n" + JSON.stringify(copy, null, 2) + "\n```");
+        msg.channel.send({ embed });
+        return;
+    }
+
+    async validate(msg: Message, args: string[], config: ConfigManager): Promise<boolean> {
+        return true;
+    }
+}
