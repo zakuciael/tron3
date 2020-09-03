7b7ad8a7402fe2e4033b4290a89d6f38fe44e1dc
Krzysztof Saczuk <42724181+zakuciael@users.noreply.github.com>
2020-09-03 15:19:45 +0200

    chore: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,125 @@
+/*
+ * Tron 3
+ * Copyright (c) 2022 Krzysztof Saczuk <zakku@zakku.eu>.
+ * MIT Licensed
+ */
+
+module.exports = {
+    root: true,
+    env: {
+        es2020: true,
+        node: true
+    },
+    extends: ["xo", "xo-typescript", "plugin:prettier/recommended", require.resolve("xo/config/plugins.cjs")],
+    plugins: [
+        "eslint-comments",
+        "n",
+        "import",
+        "unicorn",
+        "ava",
+        "no-use-extend-native",
+        "prettier"
+    ],
+    ignorePatterns: [
+        "**/node_modules/**",
+        "**/bower_components/**",
+        "flow-typed/**",
+        "coverage/**",
+        "{tmp,temp}/**",
+        "**/*.min.js",
+        "vendor/**",
+        "dist/**",
+        "tap-snapshots/*.{cjs,js}"
+    ],
+    rules: {
+        /* custom rules */
+        "new-cap": "off",
+        "capitalized-comments": "off",
+        "@typescript-eslint/naming-convention": "off",
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/unified-signatures": "off",
+        "import/no-unassigned-import": [
+            "error",
+            {
+                "allow": [
+                    "@babel/polyfill",
+                    "**/register",
+                    "**/register.*",
+                    "**/register/**",
+                    "**/register/**.*",
+                    "**/*.css",
+                    "**/*.scss",
+                    "**/*.sass",
+                    "**/*.less",
+                    "reflect-metadata",
+                    "dotenv/config.js"
+                ]
+            }
+        ],
+
+        /* xo rules */
+        "@typescript-eslint/no-duplicate-imports": [
+            "error"
+        ],
+        "import/default": [
+            "off"
+        ],
+        "import/export": [
+            "off"
+        ],
+        "import/named": [
+            "off"
+        ],
+        "import/namespace": [
+            "off",
+            {
+                "allowComputed": true
+            }
+        ],
+        "import/no-duplicates": [
+            "off"
+        ],
+        "n/no-unsupported-features/es-builtins": [
+            "error",
+            {
+                "version": ">=14.16"
+            }
+        ],
+        "n/no-unsupported-features/es-syntax": [
+            "error",
+            {
+                "ignores": [
+                    "modules"
+                ],
+                "version": ">=14.16"
+            }
+        ],
+        "n/no-unsupported-features/node-builtins": [
+            "error",
+            {
+                "version": ">=14.16"
+            }
+        ],
+        "node/file-extension-in-import": [
+            "off"
+        ],
+        "unicorn/empty-brace-spaces": [
+            "off"
+        ],
+        "unicorn/import-index": [
+            "off"
+        ],
+        "unicorn/import-style": [
+            "off"
+        ],
+        "unicorn/no-nested-ternary": [
+            "off"
+        ],
+        "unicorn/number-literal-case": [
+            "off"
+        ],
+        "unicorn/prefer-top-level-await": [
+            "off"
+        ]
+    }
+};
---
+++ README.md
@@ -0,0 +1,35 @@
+<!--suppress HtmlDeprecatedAttribute, CheckImageSize -->
+
+<h1 align="center">
+    <a href="https://github.com/zakuciael/tron3">
+        <img alt="Tron 3" src="media/logo.png" width="250" />
+    </a>
+</h1>
+
+<h6 align="center">Overview</h6>
+<h4 align="center">
+A dead-simple voice channel notifications bot for your Discord server
+</h4>
+
+<p align="center">
+  <a href="https://github.com/zakuciael/tron3/releases">
+    <img src="https://img.shields.io/github/package-json/v/zakuciael/tron3?style=flat-square" alt="Bot's version" />
+  </a>
+  <a href="https://discord.js.org/#/">
+    <img src="https://img.shields.io/github/package-json/dependency-version/zakuciael/tron3/discord.js?style=flat-square" alt="discord.js version" />
+  </a>
+  <a href="https://github.com/zakuciael/tron3/blob/main/LICENSE">
+    <img src="https://img.shields.io/github/license/zakuciael/tron3.svg?style=flat-square" alt="License" />
+  </a>
+</p>
+
+> **Note:** This branch is for the bot's `v2` version, if you want to see versions prior to `v2` go to
+> the [`legacy`](https://github.com/zakuciael/tron3/tree/legacy/) branch.
+
+## Installation
+
+This version is currently in the `development` stage, and the download is not yet available.
+
+## License
+
+MIT © [Krzysztof Saczuk \<zakku@zakku.eu\>](https://github.com/zakuciael)
