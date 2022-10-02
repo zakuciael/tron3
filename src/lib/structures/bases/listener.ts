1afa326f032059e93e5595d7e519625581d477da
Krzysztof Saczuk <zakku@zakku.eu>
2022-10-02 03:23:07 +0200

    feat: add store for event listeners

---
+++ src/lib/structures/bases/listener.ts
@@ -0,0 +1,22 @@
+/*
+ * Tron 3
+ * Copyright (c) 2022 Krzysztof Saczuk <zakku@zakku.eu>.
+ * MIT Licensed
+ */
+
+import type { EventEmitter } from "node:events";
+import { Client, ClientEvents } from "discord.js";
+import { Injectable } from "~/lib/decorators/injectable.js";
+
+@Injectable
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+export abstract class Listener<E extends keyof ClientEvents = ""> {
+    public emitter?: keyof Client | EventEmitter;
+
+    abstract readonly once: boolean;
+
+    abstract readonly event: E;
+
+    public abstract run(...args: ClientEvents[E]): Promise<void>;
+}
