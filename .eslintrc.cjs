8d3a02cab8ec1da5f7c78d11bce90f3a7d7aa01f
Krzysztof Saczuk <zakku@zakku.eu>
2022-11-01 19:53:18 +0100

    chore(eslint): prefer type imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -39,6 +39,12 @@ module.exports = {
         "@typescript-eslint/consistent-type-definitions": "off",
         "@typescript-eslint/unified-signatures": "off",
         "@typescript-eslint/class-literal-property-style": "off",
+        "@typescript-eslint/consistent-type-imports": [
+            "error",
+            {
+                "prefer": "type-imports"
+            }
+        ],
         "import/no-unassigned-import": [
             "error",
             {
